50bc883cfb43e415862dc9dd91241bff3773548a
Anton Bobrov <bobrov@vl.ru>
2010-09-27 13:32:07 +1100

    added setup info

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name     = 'snaked',
+    version  = '0.2dev',
+    author   = 'Anton Bobrov',
+    author_email = 'bobrov@vl.ru',
+    description = 'Very light and minimalist editor inspired by Scribes',
+    long_description = open('README.rst').read(),
+    zip_safe   = False,
+    install_requires = ['gsignals>=0.2'],
+    packages = find_packages(),
+    include_package_data = True,
+    namespace_packages = ['snaked', 'snaked.plugins'],
+    entry_points = {
+        'gui_scripts': [
+            'snaked = snaked.core.run:run',
+        ]
+    },
+    url = 'http://github.com/baverman/snaked',    
+)
