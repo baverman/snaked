7b641251e31e5a8aabfd77bdcc6b189a0b556bd0
Anton Bobrov <bobrov@vl.ru>
2010-09-28 16:33:25 +1100

    smart select of compound lines of one level

--- snaked/plugins/edit_and_select/__init__.py
+++ snaked/plugins/edit_and_select/__init__.py
@@ -1,6 +1,7 @@
 class Plugin(object):
     def __init__(self, editor):
         self.editor = editor
+        self.buffer = self.editor.buffer
     
     @staticmethod
     def register_shortcuts(manager):
@@ -13,7 +14,7 @@ class Plugin(object):
 
     def get_line_bounds(self, cursor=None):
         end = cursor.copy() if cursor else self.editor.cursor
-        start = self.editor.buffer.get_iter_at_line(end.get_line())
+        start = self.buffer.get_iter_at_line(end.get_line())
         
         if not end.ends_line():
             end.forward_to_line_end()
@@ -23,15 +24,13 @@ class Plugin(object):
         return start, end    
     
     def select_range(self, start, end):
-        self.editor.buffer.move_mark_by_name('insert', start)
-        self.editor.buffer.move_mark_by_name('selection_bound', end)
+        self.buffer.move_mark_by_name('insert', start)
+        self.buffer.move_mark_by_name('selection_bound', end)
     
     def delete_line(self):
-        buffer = self.editor.buffer
-        
-        buffer.begin_user_action()
-        buffer.delete(*self.get_line_bounds())
-        buffer.end_user_action()
+        self.buffer.begin_user_action()
+        self.buffer.delete(*self.get_line_bounds())
+        self.buffer.end_user_action()
     
     def cursor_on_start_or_end_whitespace(self, cursor):
         if cursor.starts_line() or cursor.ends_line():
@@ -45,13 +44,63 @@ class Plugin(object):
             return True
                  
         return False
-        
-    def smart_select(self):
-        cursor = self.editor.cursor
 
-        if self.cursor_on_start_or_end_whitespace(cursor):
-            self.select_range(*self.get_line_bounds())
-            return
-            
+    def get_whitespace(self, start):
+        end = start.copy()
+        end.forward_word_end()
+        end.backward_word_start()
+        return start.get_text(end)
         
+    def line_is_empty(self, iter):
+        if not iter.starts_line():
+            iter = iter.copy()
+            iter.set_line(iter.get_line())
+            
+        end = iter.copy()
+        end.forward_to_line_end()
+        
+        return iter.get_text(end).strip() == u''
+
+    def extend_to_block_bounds(self, start, end):
+        iter = start.copy()
+        ws = self.get_whitespace(iter)
+        while True:
+            if not iter.backward_lines(1):
+                break
+
+            if self.line_is_empty(iter) or self.get_whitespace(iter) != ws:
+                break
+
+            start = iter.copy() 
+
+        iter = end.copy()
+        iter.backward_lines(1)
+        ws = self.get_whitespace(iter)
+        while True:
+            if not iter.forward_lines(1):
+                break
                 
+            if self.line_is_empty(iter) or self.get_whitespace(iter) != ws:
+                break
+
+        end = iter 
+            
+        return start, end
+    
+    def smart_extend_selection(self, start, end):
+        if start.starts_line() and end.starts_line():
+            start, end = self.extend_to_block_bounds(start, end)  
+            
+        return start, end
+    
+    def get_smart_select(self):
+        if self.buffer.get_has_selection():        
+            return self.smart_extend_selection(*self.buffer.get_selection_bounds())
+        else:
+            cursor = self.editor.cursor
+        
+            if self.cursor_on_start_or_end_whitespace(cursor):
+                return self.get_line_bounds()
+    
+    def smart_select(self):
+        self.select_range(*self.get_smart_select())
