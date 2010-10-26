423a711699fe1af6aea7f7755dbc6683203d0671
Anton Bobrov <bobrov@vl.ru>
2010-10-27 01:50:39 +1100

    show external tool menu on <alt>X

--- snaked/plugins/external_tools/__init__.py
+++ snaked/plugins/external_tools/__init__.py
@@ -2,6 +2,8 @@ author = 'Anton Bobrov<bobrov@vl.ru>'
 name = 'External tools'
 desc = 'Allows one to define own commands'
 
+import gtk
+
 from snaked.core.prefs import register_dialog
 
 def init(manager):
@@ -9,7 +11,24 @@ def init(manager):
     register_dialog('External tools', show_preferences, 'run', 'external', 'tool', 'command')
 
 def run_tool(editor):
-    pass
+    from snaked.core.prefs import load_json_settings
+    
+    prefs = load_json_settings('external-tools.conf', {})
+    if not prefs:
+        editor.message('There is no any tool to run')
+        return
+
+    def get_coords(menu):
+        r = editor.view.get_iter_location(editor.cursor)
+        x, y = editor.view.buffer_to_window_coords(gtk.TEXT_WINDOW_WIDGET, r.x, r.y)
+        dx, dy = editor.view.get_window(gtk.TEXT_WINDOW_WIDGET).get_origin()
+        return x+dx, y+dy, False
+    
+    menu = gtk.Menu()
+    for tool in sorted(prefs):
+        menu.append(gtk.MenuItem(prefs[tool]['name'], True))
+    menu.show_all()        
+    menu.popup(None, None, get_coords, 0, gtk.get_current_event_time())
     
 def show_preferences(editor):
     from prefs import PreferencesDialog
