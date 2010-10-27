a08514b3b41d65d0b4eeeab9bc1fba226b60ec13
Anton Bobrov <bobrov@vl.ru>
2010-10-27 22:57:42 +1100

    fix: GH#17 Some bdb implementations require only str keys

--- snaked/core/prefs.py
+++ snaked/core/prefs.py
@@ -75,14 +75,20 @@ class KVSettings(object):
     def __init__(self, name):
         self.db = anydbm.open(get_settings_path(name), 'c')
     
+    def get_key(self, key):
+        if isinstance(key, unicode):
+            return key.decode('utf-8')
+        else:
+            return key
+
     def __getitem__(self, key):
-        return self.db[key]
+        return self.db[self.get_key(key)]
     
     def __contains__(self, key):
-        return self.db.has_key(key)
+        return self.db.has_key(self.get_key(key))
     
     def __setitem__(self, key, value):
-        self.db[key] = value
+        self.db[self.get_key(key)] = value
     
     def __del__(self):
         self.db.close()
