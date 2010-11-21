2f8b211a3aa6358f639d3b3d79ff767263e5da23
Anton Bobrov <bobrov@vl.ru>
2010-11-22 01:06:51 +1000

    text rewrap on right margin

--- snaked/plugins/edit_and_select/__init__.py
+++ snaked/plugins/edit_and_select/__init__.py
@@ -9,6 +9,7 @@ def init(manager):
     manager.add_shortcut('smart-select', '<alt>w', 'Selection', 'Smart anything selection', smart_select)
     manager.add_shortcut('show_offset', '<ctrl><alt>o', 'Edit',
         'Show cursor offset and column', show_offset)
+    manager.add_shortcut('wrap-text', '<alt>f', 'Edit', 'Wrap text on right margin width', wrap_text)
 
 def delete_line(editor):
     from util import get_line_bounds
@@ -16,7 +17,7 @@ def delete_line(editor):
     clipboard = editor.view.get_clipboard(gtk.gdk.SELECTION_CLIPBOARD)
     editor.buffer.select_range(*bounds)
     editor.buffer.copy_clipboard(clipboard)
-    
+
     editor.buffer.begin_user_action()
     editor.buffer.delete(*bounds)
     editor.buffer.end_user_action()
@@ -25,7 +26,29 @@ def smart_select(editor):
     from smart_select import get_smart_select
     start, end = get_smart_select(editor)
     editor.buffer.select_range(end, start)
-    
+
 def show_offset(editor):
     editor.message('offset: %d\ncolumn: %d' % (
         editor.cursor.get_offset(), editor.cursor.get_line_offset()), 3000)
+
+def wrap_text(editor):
+    buf = editor.buffer
+    if not buf.get_has_selection():
+        editor.message('Select text block to wrap.')
+        return
+
+    import textwrap
+
+    start, end = buf.get_selection_bounds()
+    start.order(end)
+    if end.starts_line():
+        end.backward_visible_cursor_position()
+
+    text = buf.get_text(start, end).decode('utf-8')
+    text = textwrap.fill(text, width=editor.view.get_right_margin_position())
+
+    buf.begin_user_action()
+    buf.place_cursor(end)
+    buf.delete(start, end)
+    buf.insert_at_cursor(text)
+    buf.end_user_action()
