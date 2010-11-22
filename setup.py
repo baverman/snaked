2385f81d3233b29ea0d4393bf5c24aa8f22d3a57
Anton Bobrov <bobrov@vl.ru>
2010-11-23 08:16:53 +1000

    0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name     = 'snaked',
-    version  = '0.4',
+    version  = '0.4.1',
     author   = 'Anton Bobrov',
     author_email = 'bobrov@vl.ru',
     description = 'Very light and minimalist editor inspired by Scribes',
@@ -27,5 +27,5 @@ setup(
         "Intended Audience :: End Users/Desktop",
         "Natural Language :: English",
         "Topic :: Text Editors"
-    ],    
+    ],
 )
