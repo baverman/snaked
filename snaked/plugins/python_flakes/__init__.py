b750c4cd105cc0b33f8c306fec2c59aeb39df3c6
Anton Bobrov <bobrov@vl.ru>
2010-11-29 09:44:25 +1000

    python_flakes: fix correct error symbol underlining

--- snaked/plugins/python_flakes/__init__.py
+++ snaked/plugins/python_flakes/__init__.py
@@ -10,41 +10,47 @@ langs = ['python']
 
 def editor_opened(editor):
     editor.connect('file-saved', on_file_saved)
-    if editor.uri and os.path.exists(editor.uri):    
+    if editor.uri and os.path.exists(editor.uri):
         add_job(editor)
-    
+
 def on_file_saved(editor):
     add_job(editor)
-    
+
 def get_tag(editor):
     table = editor.buffer.get_tag_table()
     tag = table.lookup('flakes')
     if not tag:
         tag = editor.buffer.create_tag('flakes', underline=pango.UNDERLINE_ERROR)
-    
+
     return tag
 
 def mark_problems(editor, problems):
     start, end = editor.buffer.get_bounds()
-    
+
     if editor.buffer.get_tag_table().lookup('flakes'):
         editor.buffer.remove_tag_by_name('flakes', start, end)
-    
+
     for line, name, message in problems:
         iter = editor.buffer.get_iter_at_line(line-1)
-        nstart, nend = iter.forward_search(name, gtk.TEXT_SEARCH_VISIBLE_ONLY)
-        editor.buffer.apply_tag(get_tag(editor), nstart, nend) 
-        
+        while iter.get_line() == line - 1:
+            nstart, nend = iter.forward_search(name, gtk.TEXT_SEARCH_VISIBLE_ONLY)
+            if nstart.starts_word() and nend.ends_word():
+                break
+
+            iter = nend
+
+        editor.buffer.apply_tag(get_tag(editor), nstart, nend)
+
 def get_problem_list(filename):
     import subprocess
     import re
-    
+
     stdout, stderr = subprocess.Popen(['/usr/bin/env', 'pyflakes', filename],
         stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 
     if stderr:
         raise Exception('python_flakes plugin: ' + stderr)
-    
+
     result = []
     match_name = re.compile(r"'(.*?)'")
     for m in re.finditer(r"(?m)^.*?:(?P<line>\d+):\s*(?P<message>.*$)", stdout):
@@ -52,15 +58,15 @@ def get_problem_list(filename):
         name = match_name.search(message)
         if not name:
             raise Exception("Can't parse variable name in " + message)
-        
+
         result.append((int(line), name.group(1), message))
-        
+
     return result
 
 def add_job(editor):
     from threading import Thread
     from snaked.util import idle
-    
+
     def job():
         try:
             problems = get_problem_list(editor.uri)
@@ -69,5 +75,5 @@ def add_job(editor):
             return
 
         idle(mark_problems, editor, problems)
-        
+
     Thread(target=job).start()
