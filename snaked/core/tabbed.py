e1411bac23aac8999cb0c97e513ca09a10e8c2ae
Anton Bobrov <bobrov@vl.ru>
2011-01-27 23:28:39 +1000

    Set name for main window widget for easy tuning snaked ui through gtkrc files

--- snaked/core/tabbed.py
+++ snaked/core/tabbed.py
@@ -22,6 +22,7 @@ class TabbedEditorManager(snaked.core.manager.EditorManager):
         self.panels = weakref.WeakKeyDictionary()
 
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_name('SnakedWindow')
         self.window.connect('delete-event', self.on_delete_event)
 
         # set border width handling, see self.on_state_event for more details
