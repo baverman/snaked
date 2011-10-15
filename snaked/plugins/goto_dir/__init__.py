887843cd32cb39c4b9a8c72c5aeafaa77e655ac2
Anton Bobrov <bobrov@vl.ru>
2011-10-16 00:38:52 +1100

    goto-dir

--- snaked/plugins/goto_dir/__init__.py
+++ snaked/plugins/goto_dir/__init__.py
@@ -2,12 +2,20 @@ author = 'Anton Bobrov<bobrov@vl.ru>'
 name = 'Goto dir'
 desc = "Opens file's directory"
 
-def init(manager):
-    manager.add_shortcut('goto-dir', '<ctrl><alt>l', 'File', "Opens file's directory", goto_dir)
-    
+def init(injector):
+    injector.bind_accel('editor-active', 'goto-dir', '_File/Open file\'s directory',
+        '<ctrl><alt>l', goto_dir)
+
 def goto_dir(editor):
-    import subprocess
+    import gio
     import os.path
-    
-    subprocess.Popen(['/usr/bin/env', 'xdg-open', os.path.dirname(editor.uri)]).poll()
-    editor.message('File manager started', 1000)
+
+    f = gio.file_parse_name(os.path.dirname(editor.uri))
+    ct = f.query_info(gio.FILE_ATTRIBUTE_STANDARD_CONTENT_TYPE).get_content_type()
+    ai = gio.app_info_get_default_for_type(ct, False)
+
+    if ai:
+        ai.launch([f])
+        editor.message('File manager started', 1000)
+    else:
+        editor.message('Unknown content type for launch %s' % ct)
