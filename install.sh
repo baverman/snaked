ab99d9a9a2a683ad0e3bdb8bd6a585ad539ba1c5
Anton Bobrov <bobrov@vl.ru>
2012-05-02 12:09:04 +0400

    fix: explicit python2 in install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
-python setup.py sdist
-sudo pip install -U --no-deps dist/`python setup.py --fullname`.tar.gz
+python2 setup.py sdist
+sudo pip-2.7 install -U --no-deps dist/`python2 setup.py --fullname`.tar.gz
